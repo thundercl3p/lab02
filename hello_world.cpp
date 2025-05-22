653525f6abd2e70bd7103980b503b1443eb3701e
thundercl3p <kiselyov_artyom@rambler.ru>
2025-05-22 19:55:22 +0300

    Translate comments to Russian

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 #include <string>
 int main() {
-    std::string name; // Name of user
+    std::string name; // Имя пользователя
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
-    return 0; // End of program
+    return 0; // Конец программы
 }
