5647e126fc886201a60f921d5539c9d64f011a84
thundercl3p <kiselyov_artyom@rambler.ru>
2025-05-22 19:30:30 +0300

    Ask name of user

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-using namespace std;
-int main() { 
-    cout << "Hello World!" << endl;
-    return 0;                      
+#include <string>  
+using namespace std; 
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello world from " << name << endl;
+    return 0; 
 }
