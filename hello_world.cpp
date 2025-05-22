139b642844d4393b4e001657abb1ea201dd70e51
thundercl3p <kiselyov_artyom@rambler.ru>
2025-05-22 19:37:32 +0300

    Add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,10 @@
+// Test program
 #include <iostream>
 #include <string>
 int main() {
-    std::string name;
+    std::string name; // Name of user
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from " << name << std::endl;
-    return 0;
+    return 0; // End of program
 }
